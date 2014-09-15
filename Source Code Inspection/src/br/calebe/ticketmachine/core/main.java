15d3024353742e20d7245bbd86d74f87b7fb5651
41224469 <41224469@093139-p31s302.macklabs.br>
2014-09-15 11:38:58 -0300

    Inclusão da classe main

---
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/main.java
@@ -0,0 +1,14 @@
+package br.calebe.ticketmachine.core;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 41224469
+ */
+public class main {
+    
+}
